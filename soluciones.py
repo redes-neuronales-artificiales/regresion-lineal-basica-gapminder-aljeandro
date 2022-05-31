12642c596d70523a5bbaa8b2a515eecad539d6f2
ALEJO\aleor <alejitoom5@gmail.com>
2022-05-30 21:37:05 -0500

    Entrega 4

--- soluciones.py
+++ soluciones.py
@@ -22,10 +22,10 @@ def pregunta_01():
     X = df[['fertility']].values
 
     # Imprima las dimensiones de `y`
-    print(y.shape())
+    print(y.size)
 
     # Imprima las dimensiones de `X`
-    print(X.shape())
+    print(X.size)
 
     # Transforme `y` a un array de numpy usando reshape
     y_reshaped = y.reshape(y.shape[0], y.shape[1])
