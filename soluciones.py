f27b9f84ba1b66181b6567d7889856e998913812
ALEJO\aleor <alejitoom5@gmail.com>
2022-05-30 21:59:46 -0500

    Entrega 7

--- soluciones.py
+++ soluciones.py
@@ -18,14 +18,14 @@ def pregunta_01():
     df = pd.read_csv("gm_2008_region.csv")
 
     # Asigne la columna "life" a `y` y la columna "fertility" a `X`
-    y = df[['life']]
-    X = df[['fertility']]
+    y = df[['life']].values
+    X = df[['fertility']].values
 
     # Imprima las dimensiones de `y`
-    print(y.shape)
+    print((y.shape[0],))
 
     # Imprima las dimensiones de `X`
-    print(X.shape)
+    print((X.shape[0],))
 
     # Transforme `y` a un array de numpy usando reshape
     y_reshaped = y.reshape(y.shape[0], y.shape[1])
